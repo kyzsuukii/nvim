31253cf901a1e49fa9d84416c30dba18a52ef21d
Dwiky Rizky Ananditya <kyzsuki@gmail.com>
2025-01-29 10:23:21 +0700

    update nvim-tree setup configuration
    Signed-off-by: Dwiky Rizky Ananditya <kyzsuki@gmail.com>

--- init.lua
+++ init.lua
@@ -395,8 +395,21 @@ now(function()
 	}
 
 	require("nvim-tree").setup({
+		filters = { dotfiles = false },
+		disable_netrw = true,
+		hijack_cursor = true,
+		sync_root_with_cwd = true,
+		update_focused_file = {
+			enable = true,
+			update_root = false,
+		},
 		view = {
-			width = 25,
+			width = 26,
+			preserve_window_proportions = true,
+		},
+		renderer = {
+			root_folder_label = false,
+			highlight_git = true,
 		},
 	})
 
